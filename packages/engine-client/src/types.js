941a12020793432f7bd968f8d97bd37ba110af29
Cuong Le <cuonglh2807@gmail.com>
2026-06-27 23:32:11 +0700

    chore: remove compiled .js/.js.map from src/, add to .gitignore

--- packages/engine-client/src/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Engine client types
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
--- packages/shared/src/types/api.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// ==========================================
-// API types (request/response DTOs)
-// ==========================================
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=api.types.js.map
